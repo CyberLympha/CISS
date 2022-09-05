ad2d7482f660a93bf8c62ba56486afa700bc73c6
Spongillas <askorohodov@localhost.localdomain>
2022-09-05 14:31:30 -0400

    dynamic anomaly point height

--- src/visualization.py
+++ src/visualization.py
@@ -6,15 +6,25 @@ import matplotlib.pyplot as plt
 def plot_result(data, y_test, y_pred, descr):
     plt.figure(figsize = (20,10))
 
-    plt.plot(data, alpha=0.5)
+    plt.plot(data, alpha=0.5, zorder=0)
 
     anomaly_idxs = np.where(y_test==1)
     anomaly_idxs_pred = np.where(y_pred==1)
     
+    data_max = data.max().max()
+    data_min = data.min().min()
+    
+    anomaly_marker_y = data_min + 0.5*(data_max - data_min)
+
+    
     plt.scatter(data.iloc[anomaly_idxs].index, 
-                [2000]*len(anomaly_idxs[0]), c='r', label='anomaly')
+                [anomaly_marker_y]*len(anomaly_idxs[0]), 
+                c='r', zorder=10,
+                label='anomaly')
     plt.scatter(data.iloc[anomaly_idxs_pred].index, 
-                [2100]*len(anomaly_idxs_pred[0]), c='blue', label=descr)
+                [anomaly_marker_y*0.95]*len(anomaly_idxs_pred[0]), 
+                c='blue', zorder=10,
+                label=descr)
     
     plt.legend()
     plt.title(f'{descr}. Результаты выявления аномалий')
